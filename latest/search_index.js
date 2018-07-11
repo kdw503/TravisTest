c12006c59b24e54ae05e80637119756da618902f
autodocs <autodocs>
2018-07-11 20:39:10 +0000

    build based on 2bafb8f

--- latest/search_index.js
+++ latest/search_index.js
@@ -41,11 +41,27 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "reference.html#TravisTest.window",
+    "page": "Reference",
+    "title": "TravisTest.window",
+    "category": "function",
+    "text": "window()\n\nBring up Gtk window.\n\n\n\n"
+},
+
+{
+    "location": "reference.html#TravisTest.subtract_test_wrapper",
+    "page": "Reference",
+    "title": "TravisTest.subtract_test_wrapper",
+    "category": "function",
+    "text": "subtract_test_wrapper(a,b)\n\nWrap an UnregisteredPkg.subtract_test function.\n\n\n\n"
+},
+
+{
     "location": "reference.html#Functions-1",
     "page": "Reference",
     "title": "Functions",
     "category": "section",
-    "text": "add_test"
+    "text": "add_test\nwindow\nsubtract_test_wrapper"
 },
 
 ]}

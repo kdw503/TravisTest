e0301e4865166a0c5466975ed7f099e12f4f5cd8
autodocs <autodocs>
2018-07-11 19:06:43 +0000

    build based on 7ea236e

---
+++ v0.1/search_index.js
@@ -0,0 +1,51 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "TravisTest",
+    "title": "TravisTest",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#TravisTest-1",
+    "page": "TravisTest",
+    "title": "TravisTest",
+    "category": "section",
+    "text": "TravisTest for testing Travis."
+},
+
+{
+    "location": "reference.html#",
+    "page": "Reference",
+    "title": "Reference",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "reference.html#Reference-1",
+    "page": "Reference",
+    "title": "Reference",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "reference.html#TravisTest.add_test",
+    "page": "Reference",
+    "title": "TravisTest.add_test",
+    "category": "function",
+    "text": "add_test(a,b)\n\nTest add.\n\n\n\n"
+},
+
+{
+    "location": "reference.html#Functions-1",
+    "page": "Reference",
+    "title": "Functions",
+    "category": "section",
+    "text": "add_test"
+},
+
+]}
